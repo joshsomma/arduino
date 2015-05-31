5f89e10e9af943c1294415e3d50b2e8814ee33d3
Josh Somma <josh.somma@gmail.com>
2015-05-31 23:26:40 +0100

    hourglass sketch

---
+++ hourglass/hourglass.ino
@@ -0,0 +1,36 @@
+//set up vars
+const int switchPin = 8;
+unsigned long previousTime =0;
+int switchState = 0;
+int prevSwitchState = 0; 
+int led = 2; //first led output pin
+long interval = 10000; //interval of the light switch
+void setup() {
+  //  set up output pins and input for the switch
+  for (int x = 2; x < 8; x++) {
+    pinMode(x, OUTPUT);
+  }
+  pinMode(switchPin, INPUT);
+}
+void loop() {
+  // create var and set current time
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime > interval) {
+    previousTime = currentTime;
+    digitalWrite(led,HIGH); // turn on the counter light
+    led++; //increment to the next light
+    if (led == 7) {
+      
+    }
+  }
+  switchState = digitalRead(switchPin);
+  if (switchState != prevSwitchState) {
+      for (int x = 2;x < 8; x++) {
+        digitalWrite(x, LOW);
+      }
+     led = 2;
+     previousTime = currentTime;
+    }
+  prevSwitchState = switchState;
+}
+
